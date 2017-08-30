74cb84f3410e3fda38419a9bbc36ab293f1bea0e
Road-block <dridzt.addons@hotmail.com>
2017-08-30 12:23:48 +0300

    Update Example.lua with current best practices.

--- Example.lua
+++ Example.lua
@@ -14,203 +14,202 @@
 local GuardEntry = 68   -- This is creature entry. I prefer using this just so it's set for cleanliness.
                         -- You can just set it as entry in all the RegisterUnitEvents below if you choose.
 
--- This sets us up a table specifically for this.
-GUARD = {}
+-- This sets us up a table specifically for this Script. All functions and variables scoped to this script should go in here.
+local Script = {}
 
-function UpdateHitVariable(Unit, count)
-    local sUnit = tostring(Unit)
-    GUARD[sUnit].LifeHit = count
+function Script.Guard_Spawn(Unit)
+    local sUnit = tostring(Unit)        -- tostring basically prints out the Guid. We use this with Script to store data
+    Script[sUnit] = {}                   -- Initialize everything.
+    Script[sUnit].Enraged = 0            -- We set a variable like this. We don't actually have to define it anywhere
 end
 
-function Guard_Spawn(Unit)
-    local sUnit = tostring(Unit)        -- tostring basically prints out the Guid. We use this with GUARD to store data
-    GUARD[sUnit] = {}                   -- Initialize everything.
-    GUARD[sUnit].Enraged = 0            -- We set a variable like this. We don't actually have to define it anywhere
-end
-
-function Guard_HealthPerc(Unit, hpPerc)
+function Script.Guard_HealthPerc(Unit, hpPerc)
     local sUnit = tostring(Unit)        -- Same as above, we need this to get the data for the current creature.
 
-    if GUARD[sUnit].Enraged == 0 and hpPerc < 20
-        GUARD[sUnit].Enraged = 1        -- Remember, when we set Enraged, we should set this back to 0 on death or end of combat.
+    if Script[sUnit].Enraged == 0 and hpPerc < 20
+        Script[sUnit].Enraged = 1        -- Remember, when we set Enraged, we should set this back to 0 on death or end of combat.
         -- Here we would send whatever we need to do. Chat message, spell cast, emotes, etc. Brackets aren't used in Lua
     else
     end
 end
 
-function Guard_Death(Unit, MiscUnit)
+function Script.Guard_Death(Unit, MiscUnit)
     local sUnit = tostring(Unit)        -- Same as above, we need this to get the data for the current creature.
 
-    if GUARD[sUnit].Enraged == 1
-        GUARD[sUnit].Enraged = 0        -- Setting this back since we were Enraged before death.
+    if Script[sUnit].Enraged == 1 then
+        Script[sUnit].Enraged = 0        -- Setting this back since we were Enraged before death.
+    end
 end
 
-function Guard_CombatEnd(Unit)
+function Script.Guard_CombatEnd(Unit)
     local sUnit = tostring(Unit)        -- Same as above, we need this to get the data for the current creature.
 
-    if GUARD[sUnit].Enraged == 1
-        GUARD[sUnit].Enraged = 0        -- Setting this back since we were Enraged, but combat is over and we're returning home.
+    if Script[sUnit].Enraged == 1 then
+        Script[sUnit].Enraged = 0        -- Setting this back since we were Enraged, but combat is over and we're returning home.
+    end
 end
 
 -- Lastly, we Register the events 
--- These are setup as CreatureEntry, EventId, Function Name as string.
-RegisterUnitEvent(GuardEntry, 2, "Death")
-RegisterUnitEvent(GuardEntry, 4, "CombatEnd")
-RegisterUnitEvent(GuardEntry, 5, "HealthPerc")
+-- These are setup as CreatureEntry, EventId, Function as reference.
+RegisterUnitEvent(GuardEntry, 2, Script.Guard_Death)
+RegisterUnitEvent(GuardEntry, 4, Script.Guard_CombatEnd)
+RegisterUnitEvent(GuardEntry, 5, Script.Guard_HealthPerc)
 
 --]]
 
+local Script = {}
+
 -- LUA_EVENT_INITIALIZE_CREATURE(1)
-function Spawn(Unit)
+function Script.Spawn(Unit)
 
 end
 
 -- LUA_EVENT_TERMINATE_CREATURE(2)
-function Death(Unit, Killer)
+function Script.Death(Unit, Killer)
 
 end
 
 -- LUA_EVENT_INITIALIZE_COMBAT(3)
-function CombatStart(Unit, Target)
+function Script.CombatStart(Unit, Target)
 
 end
 
 -- LUA_EVENT_TERMINATE_COMBAT(4)
-function CombatEnd(Unit)
+function Script.CombatEnd(Unit)
 
 end
 
 -- LUA_EVENT_ON_HEALTH_PERCENT(5)
-function HealthPerc(Unit, hpPerc)
+function Script.HealthPerc(Unit, hpPerc)
 
 end
 
 -- LUA_EVENT_ON_DAMAGE_DEALT(6)
-function DamageDealt(Unit, Target)
+function Script.DamageDealt(Unit, Target)
 
 end
 
 -- LUA_EVENT_ON_PHYSICAL_ATTACK(7)
-function OnPhysicalAttack(Unit, Target)
+function Script.OnPhysicalAttack(Unit, Target)
 
 end
 
 -- LUA_EVENT_ON_SPELL_CAST(8)
-function OnSpellCast(Unit, Spell, Target)
+function Script.OnSpellCast(Unit, Spell, Target)
 
 end
 
 -- LUA_EVENT_ON_DAMAGE_TAKEN(9)
-function OnDamageTaken(Unit, Attacker, Amount)
+function Script.OnDamageTaken(Unit, Attacker, Amount)
 
 end
 
 -- LUA_EVENT_ON_PHYSICAL_DAMAGE_TAKEN(10)
-function OnPhysicalDamageTaken(Unit, Attacker)
+function Script.OnPhysicalDamageTaken(Unit, Attacker)
 
 end
 
 -- LUA_EVENT_ON_SPELL_TAKEN(11)
-function OnSpellTaken(Unit, Attacker, Spell)
+function Script.OnSpellTaken(Unit, Attacker, Spell)
 
 end
 
 -- LUA_EVENT_ON_SPELL_DAMAGE_TAKEN(12)
-function OnSpellDamageTaken(Unit, Attacker, SpellId)
+function Script.OnSpellDamageTaken(Unit, Attacker, SpellId)
 
 end
 
 -- LUA_EVENT_ON_TARGET_DEATH(13)
-function OnTargetDeath(Unit, Target)
+function Script.OnTargetDeath(Unit, Target)
 
 end
 
 -- LUA_EVENT_ON_REACH_WAYPOINT(14)
-function OnReachWaypoint(Unit, WaypointId)
+function Script.OnReachWaypoint(Unit, WaypointId)
 
 end
 
 -- LUA_EVENT_ON_REACH_HOME(15)
-function OnHomeArrival(Unit)
+function Script.OnHomeArrival(Unit)
 
 end
 
 -- LUA_EVENT_ON_INITIALIZE_QUEST(16)
-function OnInitializeQuest(Unit, QuestId, Player)
+function Script.OnInitializeQuest(Unit, QuestId, Player)
 
 end
 
 -- LUA_EVENT_ON_CONCLUDE_QUEST(17)
-function OnConcludeQuest(Unit, QuestId, Player)
+function Script.OnConcludeQuest(Unit, QuestId, Player)
 
 end
 
 -- LUA_EVENT_ON_RECEIVE_EMOTE(18)
-function OnRecieveEmote(Unit, Player, EmoteId)
+function Script.OnRecieveEmote(Unit, Player, EmoteId)
 
 end
 
 -- LUA_EVENT_ON_GOSSIP_REQUEST(19)
-function OnGossipRequest(Unit, Player)
+function Script.OnGossipRequest(Unit, Player)
 
 end
 
 -- LUA_EVENT_ON_AURA_ADDED(20)
-function OnAddAura(Unit, AuraId)
+function Script.OnAddAura(Unit, AuraId)
 
 end
 
 -- LUA_EVENT_ON_AURA_REMOVED(21)
-function OnRemoveAura(Unit, AuraId)
+function Script.OnRemoveAura(Unit, AuraId)
 
 end
 
 -- LUA_EVENT_ON_HANDLE_EVENT(22)
-function OnHandleEvent(Unit, GossipItem, Player)
+function Script.OnHandleEvent(Unit, GossipItem, Player)
 
 end
 
 -- LUA_EVENT_AI_UPDATE(23)
-function AIUpdate(Unit, mapScript, timeDiff)
+function Script.AIUpdate(Unit, mapScript, timeDiff)
 
 end
 
 -- LUA_EVENT_ON_DESTRUCTION(24)
-function OnDestruction(Unit)
+function Script.OnDestruction(Unit)
 
 end
 
 -- LUA_EVENT_CLEAR_SCRIPT(25)
-function ClearScript(Unit)
+function Script.ClearScript(Unit)
 
 end
 
 --[[
 local entry = 68;
-RegisterUnitEvent(entry, 1,  "Spawn")
-RegisterUnitEvent(entry, 2,  "Death")
-RegisterUnitEvent(entry, 3,  "CombatStart")
-RegisterUnitEvent(entry, 4,  "CombatEnd")
-RegisterUnitEvent(entry, 5,  "HealthPerc")
-RegisterUnitEvent(entry, 6,  "DamageDealt")
-RegisterUnitEvent(entry, 7,  "OnPhysicalAttack")
-RegisterUnitEvent(entry, 8,  "OnSpellCast")
-RegisterUnitEvent(entry, 9,  "OnDamageTaken")
-RegisterUnitEvent(entry, 10, "OnPhysicalDamageTaken")
-RegisterUnitEvent(entry, 11, "OnSpellTaken")
-RegisterUnitEvent(entry, 12, "OnSpellDamageTaken")
-RegisterUnitEvent(entry, 13, "OnTargetDeath")
-RegisterUnitEvent(entry, 14, "OnReachWaypoint")
-RegisterUnitEvent(entry, 15, "OnHomeArrival")
-RegisterUnitEvent(entry, 16, "OnInitializeQuest")
-RegisterUnitEvent(entry, 17, "OnConcludeQuest")
-RegisterUnitEvent(entry, 18, "OnRecieveEmote")
-RegisterUnitEvent(entry, 19, "OnGossipRequest")
-RegisterUnitEvent(entry, 20, "OnAddAura")
-RegisterUnitEvent(entry, 21, "OnRemoveAura")
-RegisterUnitEvent(entry, 22, "OnHandleEvent")
-RegisterUnitEvent(entry, 23, "AIUpdate")
-RegisterUnitEvent(entry, 24, "OnDestruction")
-RegisterUnitEvent(entry, 25, "ClearScript")
+RegisterUnitEvent(entry, 1,  Script.Spawn)
+RegisterUnitEvent(entry, 2,  Script.Death)
+RegisterUnitEvent(entry, 3,  Script.CombatStart)
+RegisterUnitEvent(entry, 4,  Script.CombatEnd)
+RegisterUnitEvent(entry, 5,  Script.HealthPerc)
+RegisterUnitEvent(entry, 6,  Script.DamageDealt)
+RegisterUnitEvent(entry, 7,  Script.OnPhysicalAttack)
+RegisterUnitEvent(entry, 8,  Script.OnSpellCast)
+RegisterUnitEvent(entry, 9,  Script.OnDamageTaken)
+RegisterUnitEvent(entry, 10, Script.OnPhysicalDamageTaken)
+RegisterUnitEvent(entry, 11, Script.OnSpellTaken)
+RegisterUnitEvent(entry, 12, Script.OnSpellDamageTaken)
+RegisterUnitEvent(entry, 13, Script.OnTargetDeath)
+RegisterUnitEvent(entry, 14, Script.OnReachWaypoint)
+RegisterUnitEvent(entry, 15, Script.OnHomeArrival)
+RegisterUnitEvent(entry, 16, Script.OnInitializeQuest)
+RegisterUnitEvent(entry, 17, Script.OnConcludeQuest)
+RegisterUnitEvent(entry, 18, Script.OnRecieveEmote)
+RegisterUnitEvent(entry, 19, Script.OnGossipRequest)
+RegisterUnitEvent(entry, 20, Script.OnAddAura)
+RegisterUnitEvent(entry, 21, Script.OnRemoveAura)
+RegisterUnitEvent(entry, 22, Script.OnHandleEvent)
+RegisterUnitEvent(entry, 23, Script.AIUpdate)
+RegisterUnitEvent(entry, 24, Script.OnDestruction)
+RegisterUnitEvent(entry, 25, Script.ClearScript)
 --]]
 
